7401a0e594bac7ac756928f0bbac0a4b81a6e06c
Rishit Gupta <guptarishit52@gmail.com>
2026-06-16 17:38:03 +0530

    New features

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,33 @@
 import {Link} from "react-router-dom"
 function Navbar(){
     return(
-<nav>
-    <Link to="/">Home</Link>
-     <Link to="/search">Search</Link>
-      <Link to="/favourites">favourites</Link>
+<nav className="bg-gray-900 text-white px-8 py-4 flex justify-between items-center">
+    <h1 className="text-2xl font-bold text-yellow-400">Movie App</h1>
+
+    <div className="flex gap-6">
+        <div className="flex gap-6">
+  <Link
+    className="hover:text-yellow-400 transition duration-300"
+    to="/"
+  >
+    Home
+  </Link>
+
+  <Link
+    className="hover:text-yellow-400 transition duration-300"
+    to="/search"
+  >
+    Search
+  </Link>
+
+  <Link
+    className="hover:text-yellow-400 transition duration-300"
+    to="/favourites"
+  >
+    Favorites
+  </Link>
+</div>
+    </div>
 </nav>
     );
 }
